3dca5b109ed3922f9112ebdb26e554125f4ad9bb
FilEeaZaiR <42099557+FilEeaZaiR@users.noreply.github.com>
2019-01-16 16:38:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,10 +81,10 @@ client.on("guildMemberAdd", member => {
 
 client.on("guildMemberRemove", member => {
 
-    const bvn = member.guild.channels.find(m => m.name === "bye-bye")
-    if (!bvn) return;
-
-    bvn.send({embed: {
+    const logs = member.guild.channels.find(m => m.name === "logs");
+    if (!logs) return;
+	
+    logs.send({embed: {
             color: 0xFE6F01,
             author: {
                 name: member.user.tag,
@@ -114,41 +114,38 @@ client.on("guildMemberRemove", member => {
             }
         }
     });
-});
-
-client.on("guildMemberRemove", member => {
-
-    const logs = member.guild.channels.find(m => m.name === "logs");
-    if (!logs) return;
-
-    const embed = new Discord.RichEmbed()
-    .setColor('#FE6F01')
-    .setAuthor(member.user.tag, member.user.avatarURL)
-    .setTitle("Départ d'un utilisateur")
-    .addField("Il s'agit de", `[${member.user.tag}]`, true)
-    .addField(`Nombre de membres après le départ de __${member.user.tag}__`, member.guild.memberCount)
-    .setFooter(`ID : ${member.user.id} | FilEeaZaiR#1258`)
-    .setTimestamp()
-    logs.send(embed);
 
 });
 
 client.on("channelCreate", channel => {
 
-  if(!channel.guild) return;
-
   const logs = channel.guild.channels.find(m => m.name === "logs");
   if (!logs) return;
-
-  const embed = new Discord.RichEmbed()
-  .setColor('#FE6F01')
-  .setAuthor(client.user.tag, client.user.avatarURL)
-  .setTitle("Nouveau salon créé ! :white_check_mark:")
-  .addField("Channel créé !",`Le nom : **${channel.name}**`)
-  .addField(`Nombre de salons après l'ajout du salon **${channel.name}**`, channel.guild.channels.size)
-  .setFooter(`ID : ${channel.id} | FilEeaZaiR#1258`)
-  .setTimestamp()
-  logs.send(embed);
+	
+  logs.send({embed: {
+            color: 0xFE6F01,
+            author: {
+                name: client.user.tag,
+                icon_url: client.user.displayAvatarURL
+            },
+            title: "Nouveau salon créé ! :white_check_mark:",
+            fields: [
+            {
+                name: "Channel créé !",
+                value: `Le nom : **${channel.name}**`,
+                inline: true
+            },
+            {
+                name: `Nombre de salons après l'ajout du salon **${channel.name}**`,
+                value: channel.guild.channels.size,
+                inline: false
+            }],
+            timestamp: new Date(),
+            footer: {
+                text: `ID : ${channel.id} | FilEeaZaiR#1258`,
+            }
+        }
+    });
 
 });
 
@@ -156,83 +153,95 @@ client.on("channelDelete", channel => {
 
     const logs = channel.guild.channels.find(m => m.name === "logs");
     if (!logs) return;
-
-const embed = new Discord.RichEmbed()
-.setColor('#FE6F01')
-.setAuthor(client.user.tag, client.user.avatarURL)
-.setTitle("Un salon a été supprimé ! :white_check_mark:")
-.addField("Salon supprimé !",`Son nom : **${channel.name}**`)
-.addField(`Nombre de salons après la suppression du salon **${channel.name}**`, channel.guild.channels.size)
-.setFooter(`ID : ${channel.id} | FilEeaZaiR#1258`)
-.setTimestamp()
-logs.send(embed);
+	
+	logs.send({embed: {
+            color: 0xFE6F01,
+            author: {
+                name: client.user.tag,
+                icon_url: client.user.displayAvatarURL
+            },
+            title: "Un channel a été supprimé ! :x:",
+            fields: [
+            {
+                name: "Channel suprimé !",
+                value: `Le nom : **${channel.name}**`,
+                inline: true
+            },
+            {
+                name: `Nombre de salons après la suppression du channel **${channel.name}**`,
+                value: channel.guild.channels.size,
+                inline: false
+            }],
+            timestamp: new Date(),
+            footer: {
+                text: `ID : ${channel.id} | FilEeaZaiR#1258`,
+            }
+        }
+    });
 
 });
 
 client.on("roleCreate", role => {
     const logs = role.guild.channels.find(m => m.name === "logs");
     if (!logs) return;
-
-const embed = new Discord.RichEmbed()
-.setColor("#FE6F01")
-.setAuthor(client.user.tag, client.user.avatarURL)
-.setTitle("Un rôle a été créé ! :white_check_mark:")
-.addField("Rôle créé !", `Son nom : **${role.name}**`)
-.addField(`Nombre de rôles après l'ajout du rôle **${role.name}**`, role.guild.roles.size)
-.setFooter(`ID : ${role.id} | FilEeaZaiR#1258`)
-.setTimestamp()
-logs.send(embed);
+	
+	logs.send({embed: {
+            color: 0xFE6F01,
+            author: {
+                name: client.user.tag,
+                icon_url: client.user.displayAvatarURL
+            },
+            title: "Un rôle a été ajouté ! :white_check_mark:",
+            fields: [
+            {
+                name: "Rôle ajouté !",
+                value: `Le nom : **${channel.name}**`,
+                inline: true
+            },
+            {
+                name: `Nombre de rôles après la l'ajout du rôle **${channel.name}**`,
+                value: channel.guild.roles.size,
+                inline: false
+            }],
+            timestamp: new Date(),
+            footer: {
+                text: `ID : ${channel.id} | FilEeaZaiR#1258`,
+            }
+        }
+    });
 
 });
 
 client.on("roleDelete", role => {
     const logs = role.guild.channels.find(m => m.name === "logs");
     if (!logs) return;
-
-const embed = new Discord.RichEmbed()
-.setColor("#FE6F01")
-.setAuthor(client.user.tag, client.user.avatarURL)
-.setTitle("Un rôle a été supprimé ! :white_check_mark:")
-.addField("Rôle supprimé !", `Son nom : **${role.name}**`)
-.addField(`Nombre de rôles après la supression du rôle **${role.name}**`, role.guild.roles.size)
-.setFooter(`ID : ${role.id} | FilEeaZaiR#1258`)
-.setTimestamp()
-logs.send(embed);
+	
+	logs.send({embed: {
+            color: 0xFE6F01,
+            author: {
+                name: client.user.tag,
+                icon_url: client.user.displayAvatarURL
+            },
+            title: "Un rôle a été supprimé ! :x:",
+            fields: [
+            {
+                name: "Rôle suprimé !",
+                value: `Le nom : **${channel.name}**`,
+                inline: true
+            },
+            {
+                name: `Nombre de rôles après la suppression du rôle **${channel.name}**`,
+                value: channel.guild.roles.size,
+                inline: false
+            }],
+            timestamp: new Date(),
+            footer: {
+                text: `ID : ${channel.id} | FilEeaZaiR#1258`,
+            }
+        }
+    });
 
 });
-
-client.on("messageUpdate", (oldMessage, newMessage) => {
-  if(!newMessage.guild) return;
-    const logs = newMessage.guild.channels.find(m => m.name === "logs");
-    if (!newMessage.guild.channels.exists('name','★logs★')) return;
-  newMessage.guild.channels.find("name", "★logs★")
-    if (!logs) return console.log("Salon Logs absent!");
-    if(oldMessage.author.bot || oldMessage.cleanContent === newMessage.cleanContent) return;
-    let embed = new Discord.RichEmbed()
-    .setAuthor(newMessage.member.user.tag, newMessage.member.user.avatarURL)
-    .setColor("#FE6F01")
-    .setTitle("Un message a été modifié ! :white_check_mark:")
-    .setDescription(`Le message de ${newMessage.author} a été modifié`)
-    .addField("Message Avant", `${oldMessage.cleanContent}`)
-    .addField("Message Après", `${newMessage.cleanContent}`)
-    .setFooter(`ID : ${newMessage.member.user.id} | FilEeaZaiR#1258`)
-    .setTimestamp()
-    return logs.send(embed);
-    });
-client.on("messageDelete", (message) => {
-  if (message.author.bot) return;
-    const logs = message.guild.channels.find(m => m.name === "logs");
-    if (!logs) return;
-    let embed = new Discord.RichEmbed()
-    .setAuthor(message.member.user.tag, message.member.user.avatarURL)
-    .setColor("#FE6F01")
-    .setTitle("Un message a été supprimé ! :white_check_mark:")
-    .setDescription(`Le message de ${message.author} a été supprimé`)
-    .addField(`Message Supprimé`, `${message.cleanContent}`)
-    .setFooter(`ID : ${message.author.id} | FilEeaZaiR#1258`)
-    .setTimestamp()
-    logs.send(embed)
-  });
 
 client.on(`message`, message =>{
     
@@ -291,17 +300,27 @@ if(message.content.startsWith(prefix + "news")) {
     if(message.guild.member(message.author).roles.find("name", "-= Leaders =-")){
         let arg = message.content.split(" ").slice(1)
         let args = arg.join(" ")
-        if(!args) return message.reply(`${emoji.error} **Merci de fournir un texte.**`);
+        if(!args) return message.reply(`**Merci de fournir un texte.**`);
 
         message.delete(message.author);
-
-        var news_embed = new Discord.RichEmbed()
-        .setColor('#FE0177')
-        .setAuthor("ιהơדBot - News", client.user.avatarURL)
-        .addField("News", args)
-        .setFooter("News")
-        .setTimestamp()
-        message.channel.send(news_embed)
+        message.channel.send({embed: {
+            color: 0xFE6F01,
+            author: {
+                name: "ιהơדBot - News",
+                icon_url: client.user.displayAvatarURL
+            },
+            fields: [
+            {
+                name: "News",
+                value: args,
+                inline: true
+            }],
+            timestamp: new Date(),
+            footer: {
+                text: `ιהơדNews`,
+            }
+        }
+    });
 
     message.channel.send("<@533637357035847680>")
     .then(message => setTimeout(function(){message.delete()}, 1000))
